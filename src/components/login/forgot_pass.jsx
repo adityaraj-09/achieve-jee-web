1662ad6329814d79b726657c0067228b739af224
adityaraj-09 <itsrishabhforyou@gmail.com>
2023-10-20 13:36:47 +0530

    add

--- src/components/login/forgot_pass.jsx
+++ src/components/login/forgot_pass.jsx
@@ -5,11 +5,13 @@ import {AiOutlineInfoCircle} from "react-icons/ai"
 import {IoIosArrowBack} from "react-icons/io"
 import {HiOutlineMail} from "react-icons/hi"
 import "./fp.css"
+import { useNavigate } from 'react-router-dom'
 const FPDialog = () => {
     const [error,seterr]=useState(false)
     const [loading,setloading]=useState(false)
     const [email,setemail]=useState('')
-
+    const [sent,setsent]=useState(false)
+    const navigate=useNavigate()
 
     const sendlink=()=>{
         setloading(true)
@@ -24,16 +26,20 @@ const FPDialog = () => {
         body: JSON.stringify(data),
       })
         .then((response) => {
-          if (response.ok) {
-            throw new Error('Network response was not ok');
+            console.log(response)
+          if (!response.ok) {
+            throw new Error("Network resonse was not ok");
           }
          return response.json()
         })
         .then((responseData) => {
+            
             setloading(false)
+            setsent(true)
           
         })
         .catch((error) => {
+           console.log(error)
             setloading(false)
             seterr(true)
         });
@@ -41,7 +47,9 @@ const FPDialog = () => {
   return (
     <div className="con-fp">
         <div className="box-fp">
-            <AiOutlineInfoCircle className='ic-info'/>
+            {
+                sent?<h4>password reset link has been sent to {email}</h4>:<>
+                <AiOutlineInfoCircle className='ic-info'/>
             <h3>FORGOT PASSWORD</h3>
             <p>Enter your email and we'll send you a link to reset your password</p>
             <div className="email-input-ic">
@@ -52,10 +60,14 @@ const FPDialog = () => {
                 error && <p id='err-fp'>email not found or try again after sometime</p>
             }
             <div className="send-link-btn" onClick={sendlink}>{loading?<div className="spinner-cir"></div>:<strong>Submit</strong>}</div>
-            <div className="bck-login" onClick={()=>{}}>
+           
+                </>
+            }
+             <div className="bck-login" onClick={()=>{navigate("/",{replace:true})}}>
                 <IoIosArrowBack/>
                 back to login
             </div>
+            
         </div>
     </div>
   )
