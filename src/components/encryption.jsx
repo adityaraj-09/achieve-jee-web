fffae001eb06a21a4412ae2e915b80b1513d9764
adityaraj-09 <itsrishabhforyou@gmail.com>
2023-10-21 03:01:34 +0530

    add

--- src/components/encryption.jsx
+++ src/components/encryption.jsx
@@ -1,16 +1,18 @@
 import CryptoJS from 'crypto-js';
+import { Base64 } from 'js-base64';
 
 const secretKey = process.env.REACT_APP_ENCRYPTION_KEY; 
 
 
 export const encryptData = (data) => {
   const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
-  return encryptedData;
+  return Base64.encode(encryptedData);
 };
 
 
  export const decryptData = (encryptedData) => {
-  const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
+  const decodedEncryptedText = Base64.decode(encryptedData);
+  const bytes = CryptoJS.AES.decrypt(decodedEncryptedText, secretKey);
   if (!bytes) {
     return null;
   }
@@ -20,12 +22,13 @@ export const encryptData = (data) => {
 
 export const encryptString = (text) => {
   const encryptedText = CryptoJS.AES.encrypt(text, secretKey).toString();
-  return encryptedText;
+  return Base64.encode(encryptedText);
 };
 
 // Function to decrypt a string
 export const decryptString = (encryptedText) => {
-  const bytes = CryptoJS.AES.decrypt(encryptedText, secretKey);
+  const decodedEncryptedText = Base64.decode(encryptedText);
+  const bytes = CryptoJS.AES.decrypt(decodedEncryptedText, secretKey);
   if (!bytes) {
     return null;
   }
