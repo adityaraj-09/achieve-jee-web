5bc691613d881888d43952618ab0c93fa0cd8137
adityaraj-09 <itsrishabhforyou@gmail.com>
2023-10-22 14:11:59 +0530

    add

--- src/components/dashboard/profile.jsx
+++ src/components/dashboard/profile.jsx
@@ -4,12 +4,14 @@ import "./prof-main.css"
 import {storage} from "../../firebase"
 import {  getDownloadURL, ref,uploadBytesResumable } from "firebase/storage";
 import userEvent from '@testing-library/user-event';
-import { decryptData, encryptData } from '../encryption';
+import { decryptData, decryptString, encryptData } from '../encryption';
+import {IoMdAddCircle} from "react-icons/io"
+
 const ProfileDash = () => {
     const [selectedimgURL, setSelectedImageURL] = useState(null)
     const [selectedImage, setSelectedImage] = useState(null)
     const [uploadProgress, setUploadProgress] = useState(0);
-    const jdata=decryptData(localStorage.getItem("user"))
+    let jdata=decryptData(localStorage.getItem("user"))
     const [img,setimg]=useState(jdata["image"])
     const fileInputRef = useRef(null);
 
@@ -26,18 +28,51 @@ const ProfileDash = () => {
           }
       };
 
+     
+      const addImageToDatabse=(imageurl)=>{
+        const token=decryptString(localStorage.getItem("jwtToken"))
+        const data = {
+          id: jdata["_id"],
+         img:imageurl
+        };
+    
+        fetch('https://achieve-jee-server.onrender.com/api/upload-image', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json', 
+            'AuthGuardPass' :process.env.REACT_APP_AUTHGUARD_PASS,
+            'x-auth-token':token
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+           return response.json()
+          })
+          .then((responseData) => {
+           
+          })
+          .catch((error) => {
+           
+          });
+      }
       const resetData=(imgUrl)=>{
-        let json=JSON.parse(localStorage.getItem('user'))
+        addImageToDatabse(imgUrl) 
+        let json=decryptData(localStorage.getItem('user'))
         json.image=imgUrl
         localStorage.removeItem("user")
         const data=encryptData(json)
         localStorage.setItem("user",data)
         setimg(imgUrl)
+        
+        
       }
       const handleUpload = () => {
         if (selectedImage) {
            
-          const imageRef = ref(storage,`p-images/${jdata["_id"]}${selectedImage.name}`)
+          const imageRef = ref(storage,`p-images/${jdata["_id"]}`)
     
           const uploadTask = uploadBytesResumable(imageRef,selectedImage)
     
@@ -62,6 +97,8 @@ const ProfileDash = () => {
           );
         }
     }
+
+
   return (
     <div className="container__prof">
         <h2>Profile</h2>
@@ -73,7 +110,11 @@ const ProfileDash = () => {
 
                 <img src={selectedimgURL?selectedimgURL:img} alt="" />
                 </div>
+                <label htmlFor="up" style={{display:selectedimgURL?"none":"block"}} id="label-img">
 
+                {/* <div className="edit-btn">Choose new Photo</div> */}
+                <IoMdAddCircle/>
+                </label>
             </div>
             <div className="nea">
                 <h3>{jdata["name"]}</h3>
@@ -82,18 +123,18 @@ const ProfileDash = () => {
             </div>
             </div>
             <div className="prof-btns">
-                <label htmlFor="up" style={{display:selectedimgURL?"none":"block"}}>
-
-                <div className="edit-btn">Choose new Photo</div>
-                </label>
+                
                 {
                     selectedimgURL && <div className="edit-btn" onClick={handleUpload}>{uploadProgress===0?"Upload Photo":`Uploaded ${uploadProgress}%`}</div>
                 }
                 <input type="file"  id='up' accept="image/*" onChange={handleImageChange} style={{display:"none"}}  ref={fileInputRef}/>
-                <div className="del-btn" onClick={()=>{
+                {
+                  selectedimgURL && <div className="del-btn" onClick={()=>{
                     fileInputRef.current.value = null;
                     setUploadProgress(0)
                     setSelectedImageURL(null)}}>Delete</div>
+                }
+                
             </div>
 
         </div>
